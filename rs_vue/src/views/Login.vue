4c50f2bde76d9930ba77cb72d493819f76e7d638
mykang0318 <1378060375@qq.com>
2020-06-26 22:19:09 +0800

    添加路由导航守卫控制访问权限，对学生界面进行优化

--- rs_vue/src/views/Login.vue
+++ rs_vue/src/views/Login.vue
@@ -2,20 +2,20 @@
 <div id="login">
   <div id="form1">
     <el-card class="box-card" body-style="{padding: '0px'}">
-      <el-form ref="form" :model="form" label-width="80px">
+      <el-form ref="loginformref" :rules="loginformrules" :model="form" label-width="80px">
         <el-form-item>
           <div class="h1"><h1>重修信息系统</h1></div>
           <div class="p1"><p>rebuild information system</p></div>
           <hr>
         </el-form-item>
-        <el-form-item label="账号"style="width: 300px" class="username">
+        <el-form-item label="账号"style="width: 300px" class="username" prop="username">
           <el-input
             placeholder="请输入内容"
             v-model="form.username"
             clearable>
           </el-input>
         </el-form-item>
-        <el-form-item label="密码" style="width: 300px" class="password">
+        <el-form-item label="密码" style="width: 300px" class="password" prop="password">
           <el-input
             placeholder="请输入内容"
             v-model="form.password"
@@ -42,18 +42,34 @@
           form: {
             username: '',
             password: ''
+          },
+          //表单验证规则
+          loginformrules:{
+            username:[
+              {required: true, message: '请输入账号', trigger: 'blur'}
+            ],
+            password:[
+              {required: true, message: '请输入密码', trigger: 'blur'}
+            ]
           }
         }
       },
       methods: {
         onSubmit() {
-          if(this.form.username=='123' & this.form.password=='123'){
-            this.$router.push('/Student');
-          }else{
-            if (this.form.username=='456' & this.form.password=='456'){
+          this.$refs.loginformref.validate(valid =>{
+              if (!valid) return;
+            if(this.form.username=='123' & this.form.password=='123'){
+              window.sessionStorage.setItem("token",this.form.username);
+              this.$router.push('/Student');
+              this.$message.success('登录成功！');
+            }else if (this.form.username=='456' & this.form.password=='456'){
+              window.sessionStorage.setItem("token",this.form.username);
               this.$router.push('/Teacher');
+              this.$message.success('登录成功！');
+            }else {
+              this.$message.error('登录失败！');
             }
-          }
+          });
           console.log('submit!');
         }
       }
