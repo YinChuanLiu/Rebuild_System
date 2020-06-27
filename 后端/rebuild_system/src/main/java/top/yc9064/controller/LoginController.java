377117030ad94416ecdf5d9d79ff70dee83df846
YinChuanLiu <45043075+YinChuanLiu@users.noreply.github.com>
2020-06-28 04:38:44 +0800

    上传后端基本项目

---
+++ 后端/rebuild_system/src/main/java/top/yc9064/controller/LoginController.java
@@ -0,0 +1,17 @@
+package top.yc9064.controller;
+
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+
+}
---
+++ 后端/rebuild_system/src/main/java/top/yc9064/controller/TestController.java
@@ -0,0 +1,36 @@
+package top.yc9064.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+
+@Controller
+
+public class TestController {
+
+    @RequestMapping("/admin/upload")
+    public void toindex(@RequestParam("xlsfile") CommonsMultipartFile file, HttpServletRequest request) throws IOException {
+        System.out.println(file);
+        System.out.println("收到文件");
+        //上传路径保存设置
+        /*String path = request.getServletContext().getRealPath("/upload");
+
+        File realPath = new File(path);
+        if (!realPath.exists()) {
+            realPath.mkdir();
+        }
+        //上传文件地址
+        System.out.println("上传文件保存地址：" + realPath);
+        File file_path= new File(  "/" + file.getOriginalFilename());
+        file.transferTo(file_path);//传输文件*/
+
+    }
+
+
+}
